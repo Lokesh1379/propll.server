53b56c7e2d6b0cdc73b2187e84864750a62acfbd
Lokes1379 <chemppalokesh@gmail.com>
2026-03-09 19:01:35 +0530

    port changes

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ app.use("/api/jobs", jobRoutes);
 app.use("/api/job", emailRoutes);
 
 // local server
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on port ${PORT}`);
-  });
-}
+// if (process.env.NODE_ENV !== "production") {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
+// }
 
 module.exports = app;
